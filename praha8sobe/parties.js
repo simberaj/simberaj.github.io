162239eb2a1037a484eef807e814170aab329e58
Jan Šimbera <simbera.jan@gmail.com>
2021-05-11 11:19:39 +0200

    Praha 8 volební mapa

---
+++ praha8sobe/parties.js
@@ -0,0 +1,29 @@
+var partyColors = {
+    "ODS" : 'rgb(128,177,211)',
+    "Piráti" : 'rgb(42,42,42)',
+    "8žije" : 'rgb(255,224,0)',
+    "ANO" : 'rgb(190,186,218)',
+    "TOP 09 a STAN" : 'rgb(188,128,189)',
+    "Osmička sobě" : 'rgb(179,222,105)',
+    "Patrioti" : 'rgb(167,202,82)',
+    "ČSSD" : 'rgb(253,180,98)',
+    "KSČM" : 'rgb(251,128,114)',
+    "SPD" : 'rgb(77,41,8)',
+    "StaPP" : 'rgb(82,202,167)',
+    "SOS" : 'rgb(253,192,185)',
+};
+
+var partyResults = {
+    "ČSSD" : 4.18,
+    "KSČM" : 4.81,
+    "ANO" : 14.43,
+    "TOP 09 a STAN" : 11.29,
+    "ODS" : 17.38,
+    "Osmička sobě" : 6.06,
+    "Piráti" : 15.86,
+    "8žije" : 14.70,
+    "SPD" : 3.50,
+    "Patrioti" : 5.63,
+    "SOS" : 0.06,
+    "StaPP" : 2.11,
+};
